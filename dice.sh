cf1697228b39b87c7107782f85d4c8b93d11bb10
arunesh <arunesh.beri@bridgelabz.com>
2019-11-19 23:05:16 +0530

    check winning position

--- dice.sh
+++ dice.sh
@@ -5,13 +5,24 @@ echo "welcome"
 
 player=1
 playerPosition=0
+dieRandomNumber=0
+checkPosition=0 
 
 declare NO_PLAY=1
 declare LDDER=2
 declare SNAKE=3
 
+function winingPosition() {
+while [ $playerPosition -le 100 ]
+do
+	dice
+        echo "play"
+done
+
+}
 
 function dice() {
+
 dieRandomNumber=$(( ( RANDOM % 6 ) + 1 ))
 checkPosition=$(( ( RANDOM % 3 ) + 1 ))
 case $checkPosition in
@@ -22,9 +33,22 @@ case $checkPosition in
          playerPosition=$(( $playerPosition + $dieRandomNumber ))
          ;;
     $SNAKE)
-         playerPosition=$(( $playerPosition - $dieRandomNumber ))
+         
+         playerPosition=$( playerPostions $playerPosition $dieRandomNumber )
          ;;
 esac
 
 }
-dice
+function playerPostions() {
+         
+         if [ $1 -gt 0 ]
+	 then
+         playerPosition=$(( $playerPosition-$2 ))
+         else
+         playerPosition=0
+	 fi
+
+	 echo $playerPosition
+     
+}
+winingPosition
