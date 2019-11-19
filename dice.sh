11540b81e57955a7e41e71738c9ef21d094dd097
arunesh <arunesh.beri@bridgelabz.com>
2019-11-19 23:13:26 +0530

    refactoring code for the ladder function reusability

--- dice.sh
+++ dice.sh
@@ -2,72 +2,115 @@
 
 echo "welcome"
 
-
-player=1
+firstPlayer=1
+secondPlayer=2
 playerPosition=0
 dieRandomNumber=0
 checkPosition=0 
 dieCounter=0
-declare NO_PLAY=1
-declare LDDER=2
-declare SNAKE=3
-declare -A diedictionary 
+playerPosition1=0
+playerPosition2=0
+count=0
+ladderPosition=0
+
+NO_PLAY=1
+LADDER=2
+SNAKE=3
+
+declare -a diedictionary 
 
 function winingPosition() {
-
-    while [ $playerPosition -lt 100 ]
-    do
+    
+while [ $playerPosition -lt 100 ]
+do
+     switchPosition
+ 
+     if [ $playerPosition1 -eq 100 ]
+     then
+        echo "player one win"
+     elif [  $playerPosition2 -eq 100 ]
+     then
+        echo "player two win"   
+     fi   
         
-         dice
-       
-         if [ $playerPosition -gt 100 ]
-         then
-             playerPosition=$(( $playerPosition - $dieRandomNumber ))
-         else
-         diedictionary[$dieCounter]=$playerPosition
-         dieCounter=$(( $dieCounter + 1 ))
-         fi
-         
-       
-    done
+        count=$(( $count + 1 )) 
+done
+}
+
+function switchPosition() {
+if [ $(( $count % 2 )) -eq 0  ]
+then
+
+     playerPosition=$playerPosition1
+     dice
+     playerLadderPosition $playerPosition
+     while [ $ladderPosition == 1 ]
+     do
+     ladderPosition=0
+     dice
+     playerLadderPosition $playerPosition
+     done
+     playerPosition1=$playerPosition
+     
+else
+
+     playerPosition=$playerPosition2
+     dice
+     playerLadderPosition $playerPosition
+     while [ $ladderPosition == 1 ]
+     do
+     ladderPosition=0
+     dice
+     playerLadderPosition $playerPosition
+     done
+     playerPosition2=$playerPosition
+     
+fi
+
 }
 
 function dice() {
 
 dieRandomNumber=$(( ( RANDOM % 6 ) + 1 ))
-
 checkPosition=$(( ( RANDOM % 3 ) + 1 ))
+
 case $checkPosition in
    $NO_PLAY)
          playerPosition=$playerPosition    
          ;;
-   $LDDER)
+   $LADDER)
          playerPosition=$(( $playerPosition + $dieRandomNumber ))
+         ladderPosition=1
          ;;
     $SNAKE)
          
          playerPosition=$( playerPostions $playerPosition $dieRandomNumber )
          ;;
 esac
-
 }
-
 
 function playerPostions() {
          
-         if [ $1 -gt 0 ]
-	 then
+     if [ $1 -gt 0 ]
+     then
          playerPosition=$(( $playerPosition-$2 ))
-         else
+     else
          playerPosition=0
-	 fi
+     fi
 
-	 echo $playerPosition
-     
+     echo $playerPosition 
 }
- 
+
+function playerLadderPosition() {
+     
+     if [ $1 -gt 100 ]
+     then
+             playerPosition=$(( $playerPosition - $dieRandomNumber ))
+     else
+             diedictionary[$dieCounter]=$playerPosition
+             dieCounter=$(( $dieCounter + 1 ))
+         
+     fi
+}
+
 winingPosition
-for key in ${!diedictionary[@]}
-do 
-	echo "count:" $key " position:" ${diedictionary[$key]}
-done
