0c336eb36730bcc30b5193d34e79322226dc45bc
arunesh <arunesh.beri@bridgelabz.com>
2019-11-19 23:01:33 +0530

    calculate the player position

--- dice.sh
+++ dice.sh
@@ -2,7 +2,29 @@
 
 echo "welcome"
 
+
 player=1
 playerPosition=0
 
+declare NO_PLAY=1
+declare LDDER=2
+declare SNAKE=3
+
+
+function dice() {
 dieRandomNumber=$(( ( RANDOM % 6 ) + 1 ))
+checkPosition=$(( ( RANDOM % 3 ) + 1 ))
+case $checkPosition in
+   $NO_PLAY)
+         playerPosition=$playerPosition    
+         ;;
+   $LDDER)
+         playerPosition=$(( $playerPosition + $dieRandomNumber ))
+         ;;
+    $SNAKE)
+         playerPosition=$(( $playerPosition - $dieRandomNumber ))
+         ;;
+esac
+
+}
+dice
